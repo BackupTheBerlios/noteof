11cb6b5a66b23fc5bf19268934384faeacdee77e
dirkg <dirkg>
2008-07-18 22:57:47 +0000

    Diverse Testfälle

--- noteof/src/de/notEOF/client/test/Test.java
+++ noteof/src/de/notEOF/client/test/Test.java
@@ -1,9 +1,20 @@
 package de.notEOF.client.test;
 
+import de.notEOF.application.client.ApplicationClient;
+import de.notEOF.core.exception.ActionFailedException;
+
 public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        try {
+			ApplicationClient applClient = new ApplicationClient("127.0.0.1", 2512, null, args);
+			System.out.println(applClient.getServiceId());
+		} catch (ActionFailedException e) {
+			throw new RuntimeException(e);
+		}
+        
     }
 
 }
