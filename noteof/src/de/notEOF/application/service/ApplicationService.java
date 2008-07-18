af50ccf596c2e34d25f9f5bbb891799435d0f3b0
dirkg <dirkg>
2008-07-18 22:57:26 +0000

    Hauptsächlich getTimeOutObject auf TimeOut umgestellt.

--- noteof/src/de/notEOF/application/service/ApplicationService.java
+++ noteof/src/de/notEOF/application/service/ApplicationService.java
@@ -2,11 +2,11 @@ package de.notEOF.application.service;
 
 import java.net.Socket;
 
-import de.notEOF.application.client.ApplicationTimeout;
+import de.notEOF.application.client.ApplicationTimeOut;
 import de.notEOF.application.enumeration.ApplicationTag;
-import de.notEOF.core.communication.BaseTimeout;
 import de.notEOF.core.exception.ActionFailedException;
 import de.notEOF.core.interfaces.Service;
+import de.notEOF.core.interfaces.TimeOut;
 import de.notEOF.core.service.BaseService;
 
 public class ApplicationService extends BaseService implements Service{
@@ -24,10 +24,12 @@ public class ApplicationService extends BaseService implements Service{
     @Override
     public void processMsg(Enum receivedEnum) throws ActionFailedException {
         String bla = receivedEnum.name();
+        System.out.println(bla);
         throw new ActionFailedException(1,"");
     }
 
-    protected BaseTimeout getTimeOutObject() {
-        return new ApplicationTimeout();
+    protected TimeOut getTimeOutObject(TimeOut timeOut) {
+    	if (null != timeOut) return timeOut;
+        return new ApplicationTimeOut();
     }
 }
