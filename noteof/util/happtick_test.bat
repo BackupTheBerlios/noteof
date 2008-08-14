298029735fcbcf321269e90bb0a824d12111023a
dirkg <dirkg>
2008-08-14 20:56:29 +0000

    Erstes startscript für happtick zum Testen

---
+++ noteof/util/happtick_test.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set NOTEOF_HOME=C:\Projekte\workspace\noteof
+set CLASSPATH=%CLASSPATH%;%NOTEOF_HOME%\lib\noteof.jar
+set LIB_PATH=C:\Projekte\workspace\notioc\lib
+set CLASSPATH=%CLASSPATH%;%LIB_PATH%\notioc.jar
+set CLASSPATH=%CLASSPATH%;%LIB_PATH%\jdom.jar
+set CLASSPATH=%CLASSPATH%;c:\Projekte\workspace\happtick\lib\happtick.jar
+
+
+echo %CLASSPATH%
+java de.happtick.test.TestHapptick
+
+
+@echo on
+
+
+pause
