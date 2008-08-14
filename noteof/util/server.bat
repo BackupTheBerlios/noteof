2fb4d4948f451d48fca3b52ca389392069554756
dirkg <dirkg>
2008-08-14 20:57:04 +0000

    Startscript zum Test mit variabler Form des NOTEOF_HOME.

--- noteof/util/server.bat
+++ noteof/util/server.bat
@@ -2,14 +2,14 @@
 
 rem copy ..\..\notioc\lib\notioc.jar ..\lib
 
-set NOTEOF_HOME=C:\Projekte\workspace\noteof
-set CLASSPATH=%CLASSPATH%;%NOTEOF_HOME%\lib\noteof.jar
+set XNOTEOF_HOME=C:\Projekte\workspace\noteof
+set CLASSPATH=%CLASSPATH%;%XNOTEOF_HOME%\lib\noteof.jar
 set LIB_PATH=C:\Projekte\workspace\notioc\lib
 set CLASSPATH=%CLASSPATH%;%LIB_PATH%\notioc.jar
 set CLASSPATH=%CLASSPATH%;%LIB_PATH%\jdom.jar
 set CLASSPATH=%CLASSPATH%;c:\Projekte\workspace\happtick\lib\happtick.jar
 
 @echo on
-java de.notEOF.core.server.Server --port=3000
+java de.notEOF.core.server.Server --port=3000 --homeVar=XNOTEOF_HOME
 
 pause
