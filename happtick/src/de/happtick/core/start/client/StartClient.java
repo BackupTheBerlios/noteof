92c0f4482a534c8094fb77cd88e0f3e8154c9a69
dirkg <dirkg>
2008-08-20 21:17:04 +0000

    Erste Implementierung des StartClient, weil die Klasse jetzt benötigit wurde.

---
+++ happtick/src/de/happtick/core/start/client/StartClient.java
@@ -0,0 +1,23 @@
+package de.happtick.core.start.client;
+
+import de.notEOF.core.client.BaseClient;
+
+public class StartClient extends BaseClient {
+
+    /*
+     * suffix will sent by service later. Makes the startId's unique in the
+     * whole system.
+     */
+    private String startIdSuffix;
+
+    @Override
+    public Class<?> serviceForClientByClass() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String serviceForClientByName() {
+        return "de.happtick.core.start.service.StartService";
+    }
+}
