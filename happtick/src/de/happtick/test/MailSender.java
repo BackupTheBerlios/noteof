1d57e415331f328d0e157f544a91c5a6dbacf0b9
dirkg <dirkg>
2008-10-10 08:40:42 +0000

    Test fuer Mailversand

--- happtick/src/de/happtick/test/MailSender.java
+++ happtick/src/de/happtick/test/MailSender.java
@@ -1,15 +1,21 @@
 package de.happtick.test;
 
 import de.happtick.application.client.HapptickApplication;
+import de.happtick.core.exception.HapptickException;
 import de.notEOF.core.exception.ActionFailedException;
 import de.notEOF.mail.NotEOFMail;
 
 public class MailSender {
 
-    public static void main(String... args) throws ActionFailedException {
+    public static void main(String... args) throws HapptickException {
 
         HapptickApplication appl = new HapptickApplication(2, "localhost", 3000, args);
-        NotEOFMail mail = new NotEOFMail("Kopf", "Blabla", "Bestimmung");
+        NotEOFMail mail;
+        try {
+            mail = new NotEOFMail("Kopf", "Begriff", "Wichtiger Inhalt");
+        } catch (ActionFailedException e) {
+            throw new HapptickException(600L, "Anlegen einer Mail.", e);
+        }
         appl.sendMail(mail);
     }
 }
