df3c61ddff3dc514e0dbd38877dd6c426ed72c6b
dirkg <dirkg>
2008-12-04 23:23:26 +0000

    Schnittstelle close() hinzugefügt

--- noteof/src/de/notEOF/core/interfaces/NotEOFClient.java
+++ noteof/src/de/notEOF/core/interfaces/NotEOFClient.java
@@ -8,6 +8,8 @@ public interface NotEOFClient {
 
     public void connect(String ip, int port, TimeOut timeout) throws ActionFailedException;
 
+    public void close() throws ActionFailedException;
+
     public void sendEvent(NotEOFEvent event) throws ActionFailedException;
 
     public String getClientNetId();
